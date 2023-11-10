39d2762b9cc297b0f567b049952e3d440933fa79
Vicente Salcedo <vicentepsalcedo@gmail.com>
2023-11-10 08:50:24 -0500

    added dracula theme

---
+++ after/plugin/dracula.lua
@@ -0,0 +1,6 @@
+local dracula = require("dracula")
+dracula.setup({
+    transparent_bg = true, -- default false
+    italic_comment = true, -- default false
+})
+vim.cmd [[colorscheme dracula-soft]]
